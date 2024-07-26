6dcfde5f63529bf12ca6787fb9de52ab861c96d8
Ersandeko <ersan-deko@hotmail.com>
2024-07-26 18:25:36 +0200

    added book cover, format and letter type in the settings

--- online-biblioteka/src/app/components/Settings.jsx
+++ online-biblioteka/src/app/components/Settings.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import './Settings.css';
-import Cover from './Cover';
+import BookCover from './BookCover';
+import BookLetter from './BookLetter';
+import BookFormat from './BookFormat';
 
 const Settings = () => {
   const [activeTab, setActiveTab] = useState('policy');
@@ -8,7 +10,11 @@ const Settings = () => {
   const renderTabContent = () => {
     switch (activeTab) {
       case 'cover':
-        return <Cover />;
+        return <BookCover />;
+      case 'letter':
+        return <BookLetter />;
+      case 'format':
+        return <BookFormat />;
       // Add cases for other tabs as needed
       default:
         return <div>Select a tab to view content</div>;
@@ -25,25 +31,25 @@ const Settings = () => {
         </div>
       </div>
       <div className="py-4 text-gray-500 border-b border-gray-200 pl-8">
-        <a href="#policy" className="inline hover:text-blue-800 active-book-nav" onClick={() => setActiveTab('policy')}>
+        <a href="#policy" className={`inline hover:text-blue-800 ${activeTab === 'policy' ? 'active-book-nav' : ''}`} onClick={() => setActiveTab('policy')}>
           Polisa
         </a>
-        <a href="#categories" className="inline ml-14 hover:text-blue-800" onClick={() => setActiveTab('categories')}>
+        <a href="#categories" className={`inline ml-14 hover:text-blue-800 ${activeTab === 'categories' ? 'active-book-nav' : ''}`} onClick={() => setActiveTab('categories')}>
           Kategorije
         </a>
-        <a href="#genres" className="inline ml-14 hover:text-blue-800" onClick={() => setActiveTab('genres')}>
+        <a href="#genres" className={`inline ml-14 hover:text-blue-800 ${activeTab === 'genres' ? 'active-book-nav' : ''}`} onClick={() => setActiveTab('genres')}>
           Zanrovi
         </a>
-        <a href="#publishers" className="inline ml-14 hover:text-blue-800" onClick={() => setActiveTab('publishers')}>
+        <a href="#publishers" className={`inline ml-14 hover:text-blue-800 ${activeTab === 'publishers' ? 'active-book-nav' : ''}`} onClick={() => setActiveTab('publishers')}>
           Izdavac
         </a>
-        <a href="#cover" className="inline ml-14 hover:text-blue-800" onClick={() => setActiveTab('cover')}>
+        <a href="#cover" className={`inline ml-14 hover:text-blue-800 ${activeTab === 'cover' ? 'active-book-nav' : ''}`} onClick={() => setActiveTab('cover')}>
           Povez
         </a>
-        <a href="#format" className="inline ml-14 hover:text-blue-800" onClick={() => setActiveTab('format')}>
+        <a href="#format" className={`inline ml-14 hover:text-blue-800 ${activeTab === 'format' ? 'active-book-nav' : ''}`} onClick={() => setActiveTab('format')}>
           Format
         </a>
-        <a href="#letter" className="inline ml-14 hover:text-blue-800" onClick={() => setActiveTab('letter')}>
+        <a href="#letter" className={`inline ml-14 hover:text-blue-800 ${activeTab === 'letter' ? 'active-book-nav' : ''}`} onClick={() => setActiveTab('letter')}>
           Pismo
         </a>
       </div>
